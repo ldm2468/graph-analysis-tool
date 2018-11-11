06a385e03f2b5938cb5db888eb53677db36f9ce9
seongms <mss8087@naver.com>
2018-11-11 16:01:58 +0900

    edit matlab_plot

--- matlab_plot/plot.m
+++ matlab_plot/plot.m
@@ -1,22 +1,19 @@
-%%
-fileID_A=fopen('./plot/label-vertex.txt', 'r');
-
-
+fileID_A=fopen('./label-vertex.txt', 'r');
 formatSpec = '%d';
 A = fscanf(fileID_A, formatSpec);
-
 figure(1);
 histogram(A);
+title('label-vertex');
 xlabel('label');
 ylabel('# of vertex');
-print('-f1','./plot/label-vertex', '-dpng');
-%%
-fileID_B=fopen('./plot/degree-vertex.txt', 'r');
-B = fscanf(fileID_B, formatSpec);
+print('-f1','./label-vertex', '-dpng');
 
+fileID_B=fopen('./degree-vertex.txt', 'r');
+B = fscanf(fileID_B, formatSpec);
 figure(2);
 histogram(B);
+title('degree-vertex');
 xlabel('degree');
 ylabel('# of vertex');
-print('-f2','./plot/degree-vertex', '-dpng');
+print('-f2','./degree-vertex', '-dpng');
 
