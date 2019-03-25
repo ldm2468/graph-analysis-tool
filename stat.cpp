a1886f62b5299630a69a7ac12aa7a474976e4776
GeonmoGu <rgogor@naver.com>
2019-03-25 13:59:53 +0900

    removed USResult and DSResult in stat.h

--- stat.cpp
+++ stat.cpp
@@ -2,17 +2,6 @@
 
 namespace snu {
 	
-	void initStat(DSResult& result) {
-		result.basicstat = false;
-		result.connectstat = false;
-	}
-
-	void initStat(USResult& result) {
-		result.basicstat = false;
-		result.connectstat = false;
-		result.countstat = false;
-	}
-
 	void initStat(StatResult& result) {
 		result.basicstat = false;
 		result.connectstat = false;
--- stat.h
+++ stat.h
@@ -3,62 +3,6 @@
 
 namespace snu {
 
-	typedef struct DSResult {
-		// basic satistics
-		long long size; // number of vertices
-		long long volume; // number of edges
-		long long num_vlabel; // number of vertex label
-		long long num_elabel; // number of edge label
-		double density; // m / n^2, directed graph with loops
-		long long max_indegree; // maximum indegree
-		long long max_outdegree; // maximum outdegree
-		// size of vertex label is defined as number of vertices having that label
-		long long max_vlabel_size; // maximum size of vertex label
-		long long max_elabel_size; // maximum size of edge label
-		double reciprocity; // only defined in directed graph
-		double negativity; // ratio of nagative edges
-
-		// connectivity statistics 
-		long long num_scc; // number of strongly connected components
-		long long num_wcc; // number of weakly connected components
-		long long size_lscc; // largest size of strongly connected components
-		long long size_lwcc; // largest size of weakly connected components
-
-		// statistic check
-		bool basicstat = false;
-		bool connectstat = false;
-	} DSResult;
-
-	typedef struct USResult {
-		// basic statistics 
-		long long size; // number of vertices
-		long long volume; // number of edges
-		long long num_vlabel; // number of vertex label
-		long long num_elabel; // number of edge label
-		double density; // 2m / (N * (N + 1)), undirected graph with loops
-		double avg_degree; // average degree, 2m / n
-		long long max_degree; // maximum degree
-		// size of vertex label is defined as number of vertices having that label
-		long long max_vlabel_size; // maximum size of vertex label
-		long long max_elabel_size; // maximum size of edge label
-		double negativity; // ratio of nagative edges
-		
-		// connectivity statistics 
-		long long num_cc; // number of connected components
-		long long size_lcc; // largest size of connected components
-		long long diameter; // longest shortest paths
-
-		// counting statistics 
-		unsigned long long wedge_count; // number of wedges
-		unsigned long long claw_count; // number of claws
-		unsigned long long triangle_count; // number of triangles
-
-		// stat check 
-		bool basicstat = false;
-		bool connectstat = false;
-		bool countstat = false;
-	} USResult;
-
 	typedef struct StatResult {
 		long long size; // number of vertices
 		long long volume; // number of edges
@@ -95,8 +39,6 @@ namespace snu {
 		bool countstat = false;
 	} StatResult;
 
-	void initStat(DSResult *result);
-	void initStat(USResult *result);
 	void initStat(StatResult& result);
 }
 
