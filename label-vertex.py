ec010c5aab0f23b8d30871d7fb78d7769a236680
NamYehyun <nam.yehyun.97@gmail.com>
2019-03-30 20:12:41 +0900

    fix label-vertex.py: remove redundant if-clause

--- label-vertex.py
+++ label-vertex.py
@@ -16,24 +16,24 @@ with open('./pyplot/label-vertex.txt', mode='rt') as f:
         text=text.split()
         l=l+[text[0]]
         n=n+[int(text[1])]
-    if len(l)!=0:
-        plt.figure(1)
-        plt.bar(l,n)
-        plt.xlabel('label')
-        plt.ylabel('# of vertex')
-        plt.title('label-vertex')
-        fig = plt.gcf()
-        fpath = "./pyplot/"+str(graph_name)+"_label-vertex.png"
-        fig.savefig(fpath, dpi=300)
-        for i in range(len(n)):
-            if n[i]!=0:
-                n[i]=math.log(n[i])
-        plt.figure(2)
-        plt.bar(l,n)
-        plt.xlabel('label')
-        plt.ylabel('log(# of vertex)')
-        plt.title('label-vertex')
-        fig = plt.gcf()
-        fpath = "./pyplot/"+str(graph_name)+"_label-vertex_log.png"
-        fig.savefig(fpath, dpi=300)
+
+    plt.figure(1)
+    plt.bar(l,n)
+    plt.xlabel('label')
+    plt.ylabel('# of vertex')
+    plt.title('label-vertex')
+    fig = plt.gcf()
+    fpath = "./pyplot/"+str(graph_name)+"_label-vertex.png"
+    fig.savefig(fpath, dpi=300)
+    for i in range(len(n)):
+        if n[i]!=0:
+            n[i]=math.log(n[i])
+    plt.figure(2)
+    plt.bar(l,n)
+    plt.xlabel('label')
+    plt.ylabel('log(# of vertex)')
+    plt.title('label-vertex')
+    fig = plt.gcf()
+    fpath = "./pyplot/"+str(graph_name)+"_label-vertex_log.png"
+    fig.savefig(fpath, dpi=300)
 
