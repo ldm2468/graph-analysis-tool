aee83a18f785d7ef2173dddc886b2f249580d8a9
NamYehyun <nam.yehyun.97@gmail.com>
2019-03-31 08:51:42 +0900

    fix diameter: rewrite 'farthest'

--- diameter.cpp
+++ diameter.cpp
@@ -2,40 +2,47 @@
 
 #include <queue>
 
+
 namespace snu {
+    const unsigned long long INF = 0x7fffffffffffffff;
+
+    typedef std::pair<bool, unsigned long long> info;
+    inline bool visited(Graph::Vertex* vertex) { return static_cast<info*>(vertex->temp)->first; }
+    inline unsigned long long distance(Graph::Vertex* vertex) { return static_cast<info*>(vertex->temp)->second; }
+
     inline unsigned long long max(unsigned long long x, unsigned long long y)
     {
         return (x > y) ? x : y;
     }
 
     // Given a node, returns a pair of the farthest node and its distance form the node.
-    std::pair<Graph::Vertex*, unsigned long long> farthest(USGraph& graph, Graph::Vertex* root)
+    std::pair<Graph::Vertex*, unsigned long long> farthest(USGraph& graph, Graph::Vertex* root, bool init)
     {
-        const unsigned long long INF = 0x7fffffffffffffff;
-
-        // `temp` stores distance from the root node.
-        // Initialize those distances to inf.
-        for (auto id_vertex: graph.id_to_vertex) {
-            Graph::Vertex* vertex = id_vertex.second;
-            vertex->temp = new unsigned long long(INF);
+        if (init) {
+            for (auto id_vertex: graph.id_to_vertex) {
+                Graph::Vertex* vertex = id_vertex.second;
+                static_cast<info*>(vertex->temp)->first = false;
+                static_cast<info*>(vertex->temp)->second = INF;
+            }
         }
 
         std::queue<Graph::Vertex*> vertex_queue;
 
-        *static_cast<unsigned long long*>(root->temp) = 0;
+        static_cast<info*>(root->temp)->second = 0;
         vertex_queue.push(root);
 
         // Perform BFS and calculate distances to each node from the root node.
         while (!vertex_queue.empty()) {
             Graph::Vertex* curr = vertex_queue.front(); vertex_queue.pop();
-            unsigned long long curr_dist = *static_cast<unsigned long long*>(curr->temp);
+            static_cast<info*>(curr->temp)->first = true;
+            unsigned long long curr_dist = distance(curr);
 
             for (auto edge: curr->edges) {
                 Graph::Vertex* next = (edge->to == curr) ? edge->from : edge->to;
-                unsigned long long next_dist = *static_cast<unsigned long long*>(next->temp);
+                unsigned long long next_dist = distance(next);
 
                 if (curr_dist + 1 < next_dist) {
-                    *static_cast<unsigned long long*>(next->temp) = curr_dist + 1;
+                    static_cast<info*>(next->temp)->second = curr_dist + 1;
                     vertex_queue.push(next);
                 }
             }
@@ -46,16 +53,11 @@ namespace snu {
         unsigned long long farthest_dist = 0;
         for (auto id_vertex: graph.id_to_vertex) {
             Graph::Vertex* vertex = id_vertex.second;
-            unsigned long long dist = *static_cast<unsigned long long*>(vertex->temp);
+            unsigned long long dist = distance(vertex);
             if (dist != INF && farthest_dist < dist) {
                 farthest_dist = dist;
                 farthest_vertex = vertex;
             }            
-        }
-
-        for (auto id_vertex: graph.id_to_vertex) {
-            Graph::Vertex* vertex = id_vertex.second;
-            delete static_cast<unsigned long long*>(vertex->temp);
         }
 
         return std::make_pair(farthest_vertex, farthest_dist);
@@ -64,6 +66,11 @@ namespace snu {
     // Given a graph, returns the exact diameter of the graph.
     unsigned long long diameter(USGraph& graph)
     {
+        for (auto id_vertex: graph.id_to_vertex) {
+            Graph::Vertex* vertex = id_vertex.second;
+            vertex->temp = new info(false, INF);
+        }
+
         unsigned long long max_dist = 0;
         for (auto id_vertex: graph.id_to_vertex) {
             Graph::Vertex* vertex = id_vertex.second;
@@ -73,20 +80,46 @@ namespace snu {
             max_dist = max(dist, max_dist);
         }
 
+        for (auto id_vertex: graph.id_to_vertex) {
+            Graph::Vertex* vertex = id_vertex.second;
+            delete static_cast<info*>(vertex->temp);
+        }
+
         return max_dist;
     }
 
     // Given a graph, returns an approximate diameter of the graph.
     unsigned long long approximate_diameter(USGraph& graph)
     {
-        // Begin with an arbitray node(=root).
-        Graph::Vertex* root = graph.id_to_vertex.begin()->second;
-        
-        // Find the farthest node from the root.
-        Graph::Vertex* node = farthest(graph, root).first;
+        for (auto id_vertex: graph.id_to_vertex) {
+            Graph::Vertex* vertex = id_vertex.second;
+            vertex->temp = new info(false, INF);
+        }
+
+        // Construct candidate set. 
+        // Candidate set includes one node per connected component.
+        std::set<Graph::Vertex*> candidate_nodes;
+        for (auto id_vertex: graph.id_to_vertex) {
+            Graph::Vertex* vertex = id_vertex.second;
+            if (visited(vertex)) {
+                continue;
+            }
+            candidate_nodes.insert(farthest(graph, vertex, false).first);
+        }
+
+        unsigned long long max_dist = 0;
+        for (auto vertex: candidate_nodes) {
+            unsigned long long dist = farthest(graph, vertex).second;
+            max_dist = max(dist, max_dist);
+        }
+
+        for (auto id_vertex: graph.id_to_vertex) {
+            Graph::Vertex* vertex = id_vertex.second;
+            delete static_cast<info*>(vertex->temp);
+        }
 
         // Return the distance between the node and the farthest node from the node.
-        return farthest(graph, node).second;
+        return max_dist;
     }
 }
 
--- diameter.h
+++ diameter.h
@@ -1,12 +1,14 @@
 #ifndef DIAMETER_H
 #define DIAMETER_H
 
+#include <set>
 #include <utility>
+
 #include "graph.h"
 
 namespace snu {
     // Given a graph and a node, returns the farthest node and its distance from the node.
-    std::pair<Graph::Vertex*, unsigned long long> farthest(USGraph& graph, Graph::Vertex* root);
+    std::pair<Graph::Vertex*, unsigned long long> farthest(USGraph& graph, Graph::Vertex* root, bool init = true);
 
     // Given a graph, returns the exact diameter of the graph.
     unsigned long long diameter(USGraph& graph);
