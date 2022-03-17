28978a5c326535c23b2d4fd65b8355e187da1064
Dongmin Lee <ldm2468@snu.ac.kr>
2022-03-18 02:00:57 +0900

    Add matrix class

---
+++ matrix.cpp
@@ -0,0 +1,20 @@
+#include "matrix.h"
+
+snu::Matrix::Matrix(const size_t r, const size_t c) : rows(r), cols(c) {
+    dat.resize(r * c);
+}
+
+double &snu::Matrix::at(int r, int c) {
+    return dat[r * cols + c];
+}
+
+void snu::Matrix::mul(const std::vector<double> &v, std::vector<double> &res) {
+    res.reserve(rows);
+    for (int r = 0; r < rows; r++) {
+        double acc = 0;
+        for (int c = 0; c < cols; c++) {
+            acc += dat[r * cols + c] * v[c];
+        }
+        res[r] = acc;
+    }
+}
