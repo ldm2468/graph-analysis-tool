1dad977414795f96954f41c3a77b2d91b7ccc9de
Dongmin Lee <ldm2468@snu.ac.kr>
2022-03-18 02:02:26 +0900

    Implement PageRank algorithm

--- eigencentrality.cpp
+++ eigencentrality.cpp
@@ -1,21 +1,49 @@
 #include "eigencentrality.h"
+#include "matrix.h"
 
 namespace snu {
-    typedef struct VertexMetadata {
-        double prob;
-    } VertexMetadata;
-
-    static VertexMetadata *getMetadata(Graph::Vertex *v) {
-        return (VertexMetadata *) v->temp;
-    }
-
     void pageRank(Graph &graph, StatResult &result) {
-        for (auto& pair: graph.id_to_vertex) {
-            pair.second->temp = new VertexMetadata();
-        }
+        int n = (int) graph.id_to_vertex.size();
+        auto mat = new Matrix(n, n);
+        std::vector<double> prob, mul;
+        prob.resize(n);
 
         for (auto& pair: graph.id_to_vertex) {
-            delete getMetadata(pair.second);
+            auto g = pair.second;
+            prob[g->id] = 1;
+
+            if (!g->edges.empty()) {
+                double w = 1. / (double) g->edges.size();
+                for (auto& edge: g->edges) {
+                    auto to = edge->to == g ? edge->from : edge->to;
+                    mat->at((int) to->id, (int) g->id) = w;
+                }
+            }
         }
+
+        bool not_finished = true;
+        int iterations = 0;
+        double damper = 1. - DAMPING_FACTOR;
+        while (not_finished) {
+            iterations++;
+            not_finished = false;
+            mat->mul(prob, mul);
+            for (int i = 0; i < prob.size(); i++) {
+                double next = damper + DAMPING_FACTOR * mul[i];
+                if ((next > prob[i] ? next - prob[i] : prob[i] - next) > CONVERGENCE_TEST) {
+                    not_finished = true;
+                }
+                prob[i] = next;
+            }
+        }
+
+        result.max_pagerank = 0;
+        for (int i = 0; i < n; i++) {
+            if (prob[i] > result.max_pagerank) {
+                result.max_pagerank = prob[i];
+            }
+        }
+
+        result.eigencentralitystat = true;
     }
 }
--- eigencentrality.h
+++ eigencentrality.h
@@ -4,6 +4,9 @@
 #include "graph.h"
 #include "stat.h"
 
+#define DAMPING_FACTOR 0.85
+#define CONVERGENCE_TEST 1E-6
+
 namespace snu {
     void pageRank(Graph &graph, StatResult &result);
 }
